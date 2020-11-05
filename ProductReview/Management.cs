d67275bf4bc08dd9e7cb0d2413b50a17927bf2cc
sharyu raut <sharyukraut210298@gmail.com>
2020-11-05 23:49:53 +0530

    [Sharyu] Add. Displaying top 3 records

---
+++ ProductReview/Management.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Linq;
+
+namespace ProductReview
+{
+    class Management
+    {
+        public readonly DataTable dt = new DataTable();
+
+        public void TopRecords(List<ProductReviewClass> ProductReviewlist)
+        {
+            var recordedData = (from productReviews in ProductReviewlist
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3).ToList();
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductId + " UserID: " + list.UserId + " Rating: " + list.Rating + " Review: " + list.Review + " isLike: " + list.IsLike);
+            }
+        }
+    }
+}
--- ProductReview/Program.cs
+++ ProductReview/Program.cs
@@ -20,6 +20,20 @@ namespace ProductReview
                 new ProductReviewClass() { ProductId = 5, UserId = 3, Rating = 5, Review = "Good", IsLike = false },
                 new ProductReviewClass() { ProductId = 6, UserId = 3, Rating = 5, Review = "Good", IsLike = true },
                 new ProductReviewClass() { ProductId = 7, UserId = 6, Rating = 5, Review = "Good", IsLike = false },
+                new ProductReviewClass(){ProductId=21,UserId=13,Rating=4,Review="Great",IsLike=true},
+                new ProductReviewClass(){ProductId=8,UserId=15,Rating=3,Review="Good",IsLike=true},
+                new ProductReviewClass(){ProductId=9,UserId=17,Rating=2,Review="Okay",IsLike=true},
+                new ProductReviewClass(){ProductId=10,UserId=18,Rating=1,Review="Poor",IsLike=true},
+                new ProductReviewClass(){ProductId=11,UserId=19,Rating=5,Review="Super",IsLike=true},
+                new ProductReviewClass(){ProductId=12,UserId=21,Rating=4,Review="Great",IsLike=true},
+                new ProductReviewClass(){ProductId=13,UserId=23,Rating=10,Review="Good",IsLike=true},
+                new ProductReviewClass(){ProductId=14,UserId=25,Rating=2,Review="Okay",IsLike=true},
+                new ProductReviewClass(){ProductId=15,UserId=27,Rating=1,Review="Poor",IsLike=true},
+                new ProductReviewClass(){ProductId=16,UserId=29,Rating=5,Review="Super",IsLike=true},
+                new ProductReviewClass(){ProductId=17,UserId=31,Rating=4,Review="Great",IsLike=true},
+                new ProductReviewClass(){ProductId=18,UserId=33,Rating=7,Review="Good",IsLike=true},
+                new ProductReviewClass(){ProductId=19,UserId=35,Rating=2,Review="Okay",IsLike=true},
+                new ProductReviewClass(){ProductId=20,UserId=37,Rating=1,Review="Poor",IsLike=true},
             };
 
             foreach (var list in productReviewList)
@@ -27,6 +41,10 @@ namespace ProductReview
                 Console.WriteLine("ProductID: " + list.ProductId + " " + "UserID: " + list.UserId + " " + "Rating: " + list.Rating + " " + "Review: " + list.Review + " " + "IsLike: " + list.IsLike);
             }
 
+            Console.WriteLine("Displaying top 3 records---------------");
+
+            Management mg = new Management();
+            mg.TopRecords(productReviewList);
         }
 
 
