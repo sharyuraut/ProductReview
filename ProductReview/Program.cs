ed04af4b8d2e78411e0a701e1957162178020c53
sharyu raut <sharyukraut210298@gmail.com>
2020-11-05 09:28:05 +0530

    [Sharyu] Add. displaying product names

--- ProductReview/Program.cs
+++ ProductReview/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
 
 namespace ProductReview
 {
@@ -6,7 +8,29 @@ namespace ProductReview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("LINQ-Product Review Program!");
+
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "A");
+            table.Rows.Add("2", "B");
+            table.Rows.Add("3", "C");
+
+            Program inst = new Program();
+            inst.DisplayProducts(table);
+
+        }
+
+        private void DisplayProducts(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("Product Names: ");
+            foreach(string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
         }
     }
 }
